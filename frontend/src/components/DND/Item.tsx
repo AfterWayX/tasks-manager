3ac7af7974eea67c617d2a62170ad1e9c52eac64
dberegoi <dberegoi@qoobus.com>
2023-04-04 15:16:29 +0300

    formatting

--- frontend/src/components/DND/Item.tsx
+++ frontend/src/components/DND/Item.tsx
@@ -9,7 +9,8 @@ interface DNDItemInterface {
 }
 
 export default function DNDItem({ task, handleChange }: DNDItemInterface) {
-    const [isDetails, setIsDetails] = useState(false)
+    const [isDetails, setIsDetails] = useState(false);
+
     const handleDelete = async () => {
         TasksService.deleteTaskFromList(task.id);
         setTimeout(() => handleChange(), 1)
@@ -33,10 +34,24 @@ export default function DNDItem({ task, handleChange }: DNDItemInterface) {
         <div className="grid grid-cols-12 w-full border border-gray-300 rounded-lg p-1.5 justify-between">
             <div className={`${isDetails ? "col-span-full" : "col-span-8"}`}>
                 <div className="flex items-center">
-                    <h3 className="text-lg">{task.title}</h3>
-                    <button onClick={() => setIsDetails((prev) => !prev)} className="text-center text-xs text-gray-500 ml-2 mt-1">{isDetails ? "Hide details" : "View details"}{'>'}</button>
+                    <h3 className="text-lg">
+                        {task.title}
+
+                    </h3>
+                    <button
+                        onClick={() => setIsDetails((prev) => !prev)}
+                        className="text-center text-xs text-gray-500 ml-2 mt-1"
+                    >
+                        {isDetails ? "Hide details" : "View details"}{'>'}
+                    </button>
                 </div>
-                <div className={`w-full text-sm text-opacity-90 text-black relative group pr-1 z-[999] ${isDetails ? "min-h-max" : "truncate"}`}>{task.description}
+                <div
+                    className={`
+                    w-full text-sm text-opacity-90 text-black relative group pr-1 z-[999] 
+                    ${isDetails ? "min-h-max" : "truncate"}
+                    `}
+                >
+                    {task.description}
                 </div>
             </div>
             <div className={`flex gap-2 ${isDetails ? "col-span-full pt-2" : "col-span-4 flex-col"}`}>
